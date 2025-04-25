558c233d07720a539de67b96af41ee57651b8837
Snoopy <khaled1907>
2025-04-25 10:49:36 +0300

    add localization folders and translate arabic and english words for app

---
+++ lib/taps/quran_tap/quran_screeen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class Quran_tab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('QURAN'),
+        ],
+      ),
+    );
+  }
+}
